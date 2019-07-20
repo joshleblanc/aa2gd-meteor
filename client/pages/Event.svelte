598dff40ec624d9b0aa202ea64110c773b71c1f7
Joshua LeBlanc <me@jleb.ca>
2019-07-20 14:13:15 -0300

    added user list/sign up

--- client/pages/Event.svelte
+++ client/pages/Event.svelte
@@ -7,14 +7,23 @@
   import Button from '../components/Button';
   import { Meteor } from 'meteor/meteor';
   import StyledPaper from '../components/StyledPaper';
+  import { User } from '/lib/User';
+  import List from '../components/List';
+  import ListItem from '../components/ListItem';
+  import ListItemText from '../components/ListItemText';
+  import ListItemPrimaryText from '../components/ListItemPrimaryText';
+  import ListItemAvatar from '../components/ListItemAvatar';
+  import Avatar from '../components/Avatar';
 
   export let id;
 
   let event;
+  let users = [];
   function computation() {
     event = Event.findOne({
       _id: id
     });
+    users = event.users().fetch();
   }
 
   function handleDelete() {
@@ -24,7 +33,7 @@
   }
 
   function handleSignupToggle() {
-
+    event.registerUser(Meteor.userId());
   }
 </script>
 
@@ -34,12 +43,29 @@
       {event.date.toLocaleString()}
     </HeaderPaper>
     <StyledPaper title="Actions">
-      <Button variant="primary" on:click={handleSignupToggle}>RSVP</Button>
+      {#if event.userIds.includes(Meteor.userId())}
+        <Button variant="warning" on:click={handleSignupToggle}>Cancel sign up</Button>
+      {:else}
+        <Button variant="primary" on:click={handleSignupToggle}>Sign up</Button>
+      {/if}
+      
       {#if event.creatorId === Meteor.userId()}
         <Button variant="error" on:click={handleDelete}>Delete</Button>
       {/if}
     </StyledPaper>
-    <StyledPaper title="Users">
+    <StyledPaper title="Users signed up">
+      <List>
+        {#each users as user}
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar src={user.avatarUrl()} />
+            </ListItemAvatar>
+            <ListItemText>
+              <ListItemPrimaryText>{user.services.discord.username}</ListItemPrimaryText>
+            </ListItemText>
+          </ListItem>
+        {/each}
+      </List>
     </StyledPaper>
   </Tracker>
 {/if}
