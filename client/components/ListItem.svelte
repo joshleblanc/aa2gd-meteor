5098b07ab1b63eed5e27ab960038ac7afcec3d64
Joshua LeBlanc <jleblanc@omics.com>
2020-03-17 14:41:42 -0300

    chore: consolidate padding style

--- client/components/ListItem.svelte
+++ client/components/ListItem.svelte
@@ -5,16 +5,13 @@
 
 <style>
     li {
-        padding-left: 16px;
-        padding-right: 16px;
         width: 100%;
         display: flex;
         position: relative;
         box-sizing: border-box;
         text-align: left;
         align-items: center;
-        padding-top: 8px;
-        padding-bottom: 8px;
+        padding: 8px 16px;
         justify-content: flex-start;
         text-decoration: none;
     }
